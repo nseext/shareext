cb0a593f2b0f88e4ccbd5657e72e9e1d1fdf53c6
unknown <anupamurl@gmail.com>
2017-07-05 00:20:59 +0530

    updates

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -22,14 +22,52 @@ export class HomeComponent implements OnInit {
    *
    * @param {NameListService} nameListService - The injected NameListService.
    */
-  constructor(public nameListService: NameListService) {}
+  constructor(public nameListService: NameListService) { }
 
   /**
    * Get the names OnInit
    */
   ngOnInit() {
     this.getNames();
+    console.log(this.calculator(530.85, 512.05, 526.70))
   }
+
+
+  calculator(high: any, low: any, close: any) {
+
+
+    var pivot = (high + low + close) / 3.0
+    var range = high - low
+    var h5 = (high / low) * close
+    var h4 = close + (high - low) * 1.1 / 2.0
+    var h3 = close + (high - low) * 1.1 / 4.0
+    var h2 = close + (high - low) * 1.1 / 6.0
+    var h1 = close + (high - low) * 1.1 / 12.0
+    var l1 = close - (high - low) * 1.1 / 12.0
+    var l2 = close - (high - low) * 1.1 / 6.0
+    var l3 = close - (high - low) * 1.1 / 4.0
+    var l4 = close - (high - low) * 1.1 / 2.0
+    var h6 = h5 + 1.168 * (h5 - h4)
+    var l5 = close - (h5 - close)
+    var l6 = close - (h6 - close)
+
+
+    return ({
+      paivo: pivot,
+      r1: h1,
+      r2: h2,
+      r3: h3,
+      r4: h4, 
+      s1: l1,
+      s2: l2,
+      s3: l3,
+      s4: l4  
+    })
+
+
+  }
+
+
 
   /**
    * Handle the nameListService observable
@@ -37,8 +75,8 @@ export class HomeComponent implements OnInit {
   getNames() {
     this.nameListService.get()
       .subscribe(
-        names => this.names = names,
-        error => this.errorMessage = <any>error
+      names => this.names = names,
+      error => this.errorMessage = <any>error
       );
   }
 
